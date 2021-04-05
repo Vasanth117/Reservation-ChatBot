9d93cf492712d8999eab335184eb9a1049374de2
vasanth117 <vasanthakumara117@gmail.com>
2021-04-05 11:01:10 +0530

    .....
    .......
    ......

---
+++ bot.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+from chatterbot import ChatBot
+
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+bott = ChatBot("Vasj ChatBot")
+trainer = ChatterBotCorpusTrainer(bott)
+
+trainer.train("data/data.yml")
+trainer.train("chatterbot.corpus.english")
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+	return render_template("index.html")
+
+@app.route("/get")
+
+def get_bot_response():
+	userText = request.args.get('msg')
+	return str(bott.get_response(userText))
+
+if __name__ == "__main__":
+	app.run()
